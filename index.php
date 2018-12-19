31ba3e4939624b04346a090df41d0f65d5b2d24d
ajnyga <ajnyga@gmail.com>
2018-12-19 10:53:56 +0200

    oops

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @defgroup plugins_generic_useGlobalSender useGlobalSender Plugin
+ */
+ 
+/**
+ * @file plugins/generic/useGlobalSender/index.php
+ *
+ * Copyright (c) 2014-2018 Simon Fraser University
+ * Copyright (c) 2003-2018 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @ingroup plugins_generic_useGlobalSender
+ * @brief Wrapper for useGlobalSender plugin.
+ *
+ */
+
+require_once('UseGlobalSenderPlugin.inc.php');
+
+return new UseGlobalSenderPlugin();
+
+?>
